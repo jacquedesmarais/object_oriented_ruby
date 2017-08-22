b13b70bb868c0ada90d56a5577cb8bc40d9e9ca0
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-08-21 21:26:42 -0500

    edits to employee and store item hashes

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+item_1 = {item: "shirt", color: "blue", size: "xs"}
+item_2 = {:item => "pants", :color => "black", :size => "28"}
+item_3 = {item: "shoes", color: "brown", size: "8"}
+
+puts "#{item_1[:item]} #{item_1[:color]} #{item_1[:size]}"
